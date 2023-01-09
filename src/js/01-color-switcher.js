9483fbaa14b5d9a4c03fa9885869833e6700acb0
Natalia Karpenko <106306905+natalia-no-war@users.noreply.github.com>
2023-01-09 14:18:09 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const bodyRef = document.querySelector('body');
+const startBtnRef = document.querySelector('button[data-start]');
+const stopBtnRef = document.querySelector('button[data-stop]');
+let timerId = null;
 
+startBtnRef.addEventListener('click', handleStartBtnClick);
+stopBtnRef.addEventListener('click', handleStopBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyColor() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+}
+
+function handleStartBtnClick() {
+  timerId = setInterval(changeBodyColor, 1000);
+  startBtnRef.disabled = true;
+  stopBtnRef.disabled = false;
+}
+
+function handleStopBtnClick() {
+  clearInterval(timerId);
+  startBtnRef.disabled = false;
+  stopBtnRef.disabled = true;
+}
